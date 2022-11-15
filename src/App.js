54ef655d5f40782922de270ad61f5a45eb99e171
Nitesh Pawar <pawarnitesh200@gmail.com>
2022-11-15 23:23:55 +0530

    news website

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Pagination from "./Component/Pagination";
+import Search from "./Component/Search";
+import Stories from "./Component/Stories";
+import "./App.css";
+
+const App = () => {
+  return (
+    <>
+      <Search />
+      <Pagination />
+      <Stories />
+    </>
+  );
+};
+
+export default App;
